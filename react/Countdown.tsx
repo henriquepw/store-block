8182ff01c8208d1c4b9b3a381a9ccc4ed164c878
Henrique Miranda <enrque.ns@gmail.com>
2020-08-09 19:15:37 -0300

    :sparkles: feat: add targetData on countdown

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
-interface CountdownProps { }
+interface CountdownProps {
+  targetDate: string
+}
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate }) => {
   return (
     <div>
-      <h1>Teste Countdown</h1>
+      <h1>{targetDate}</h1>
     </div>
   )
 }
@@ -14,7 +16,14 @@ Countdown.schema = {
   title: 'editor.countdown.title',
   description: 'editor.countdown.description',
   type: 'object',
-  properties: {},
+  properties: {
+    targetDate: {
+      title: 'Data final',
+      description: 'Data final utilizada no contador',
+      type: 'string',
+      default: null,
+    },
+  },
 }
 
 export default Countdown
