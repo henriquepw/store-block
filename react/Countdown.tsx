c101046b06df284dbc629cff35d8aa0d0568c34c
Henrique Miranda <enrque.ns@gmail.com>
2020-08-10 12:29:28 -0300

    :bulb: chore: dealing with intl

--- react/Countdown.tsx
+++ react/Countdown.tsx
@@ -1,30 +1,37 @@
 import React, { useState } from 'react'
-import { TimeSplit } from './typings/global'
-import { tick } from './utils/time'
+import { FormattedMessage } from 'react-intl'
 
 import { useCssHandles } from 'vtex.css-handles'
 
+import { TimeSplit } from './typings/global'
+import { tick } from './utils/time'
+
 interface CountdownProps {
+  title: string
   targetDate: string
 }
 const DEFAULT_TARGET_DATE = (new Date('2020-08-11')).toISOString()
 
-const CSS_HANDLES = ['countdown']
+const CSS_HANDLES = ['container', 'title', 'countdown']
 
-const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ targetDate = DEFAULT_TARGET_DATE }) => {
+const Countdown: StorefrontFunctionComponent<CountdownProps> = ({ title, targetDate = DEFAULT_TARGET_DATE }) => {
   const [timeRemaining, setTime] = useState<TimeSplit>({
       hours: '00',
       minutes: '00',
       seconds: '00'
     })
 
+  const titleText = title || <FormattedMessage id="countdown.title" />
   const handles = useCssHandles(CSS_HANDLES)
 
   tick(targetDate, setTime)
 
   return (
-    <div className={`${handles.countdown} t-heading-2 fw3 w-100 c-muted-1 db tc`}>
-      <h1>{`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</h1>
+    <div className={`${handles.container} t-heading-2 fw3 w-100 c-muted-1`}>
+      <div className={`${handles.title} db tc`}>{titleText}</div>
+      <div className={`${handles.countdown} db tc`}>
+        {`${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}
+      </div>
     </div>
   ) 
 }
@@ -34,6 +41,11 @@ Countdown.schema = {
   description: 'editor.countdown.description',
   type: 'object',
   properties: {
+    title: {
+      title: 'Sou um título',
+      type: 'string',
+      default: null,
+    },
     targetDate: {
       title: 'Data final',
       description: 'Data final utilizada no contador',
